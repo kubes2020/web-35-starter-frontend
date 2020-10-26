323fae8ddd92671804b061dfed4b0c46259feada
Brian Kubes <briankubes@gmail.com>
2020-10-25 18:37:39 -0700

    what i said earlier

---
+++ src/component/DogAdd.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const DogAdd = (props) =>{
+    const [initialFormState] = useState({
+        breed: "",
+        imageUrl: ""
+    })
+
+    const [dog, setDog] = useState(initialFormState)
+
+    const changeHandler = (e) => {
+        setDog({
+            ...dog, [e.target.name]: e.target.value
+        })
+    }
+
+    const addDog = (e) => {
+        e.preventDefault()
+        axios.post(`${props.baseUrl}/dogs`, dog)
+        .then(res => {
+            props.refreshHandler()
+            setDog(initialFormState)
+        })
+        .catch(err=>{
+            console.log("error with addDog", err)
+        })
+    }
+   
+    return(
+        <>
+        <h2>Add Breed</h2>
+        <form onSubmit={addDog}>
+            <label>Breed
+                <input type="text" name="breed" value={dog.breed} onChange={changeHandler} >
+                </input>
+            </label>
+            <label>Url to Image
+                <input type="text" name="imageUrl" value={dog.imageUrl} onChange={changeHandler} >
+                </input>
+            </label>
+            <button>Submit</button>
+        </form>
+        </>
+    )
+
+}
+
+export default DogAdd
